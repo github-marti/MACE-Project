01d16744ba4f7c2849f1cb0008eb82e0c0282d9c
Marti <52050778+github-marti@users.noreply.github.com>
2019-10-28 13:33:36 -0700

    created routes for accessing the vocab database in vocab-api-routes

--- routes/vocab-api-routes.js
+++ routes/vocab-api-routes.js
@@ -1,1 +1,21 @@
 var db = require("../models");
+
+module.exports = function(app) {
+    app.get("/api/vocab", function(req, res) {
+        db.Vocab.findAll()
+        .then(results => {
+            console.log("vocabulary sent", results[0].dataValues);
+            res.json(results);
+        });
+    });
+
+    app.post("/vocab/:vocab/", function(req, res) {
+        db.Vocab.create({
+            word: req.params.vocab,
+            language: req.body.language
+        })
+        .then(results => {
+            res.json(results);
+        });
+    });
+};
