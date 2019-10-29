c0ebf4bd0934209f258c292d40af46c724099596
Marti <52050778+github-marti@users.noreply.github.com>
2019-10-29 10:52:10 -0700

    added vocabList.js with event handler for making an ajax post request to save new word to database

---
+++ public/js/vocabList.js
@@ -0,0 +1,21 @@
+$("#save-btn").on("click", function() {
+    // newWord must refer to the vocab word that the user wants to save
+    let newWord = $("#new-word").text();
+
+    // newLang must refer to the language the word is in
+    let newLang = $("#new-word").data("lang-id");
+
+    // newDifficulty must refer to the difficulty level the user chooses
+    let newDifficulty = $("#difficulty").val();
+
+    let newVocab = {
+        word: newWord,
+        difficulty: newDifficulty,
+        LanguageId: newLang
+    };
+
+    $.post(`/vocab/${newWord}`, newVocab, function(data) {
+        console.log(data);
+        console.log("Word successfully saved!");
+    });
+});
