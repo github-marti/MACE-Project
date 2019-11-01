445f1719f312dd15d1dcd665d120a877596b8c17
Emi Rezaei <tomoyose.emi@gmail.com>
2019-11-01 13:14:32 -0700

    add

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ require("./routes/vocab-api-routes.js")(app);
 require("./routes/vocablist-api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync({ force: false }).then(function() {
+db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
